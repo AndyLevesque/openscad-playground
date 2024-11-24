13e680f782fd1b2ffd84ab6c04fcbd0187cee45d
Andy Levesque <andy.m.levesque@gmail.com>
2024-11-24 14:34:23 -0600

    more attempts at build fix

--- build-openscad-wasm.sh
+++ build-openscad-wasm.sh
@@ -13,7 +13,7 @@ fi
 
 # Removed '-it' from the docker run commands
 docker run --rm  -v "$OPENSCAD_DIR":/src:rw --platform=linux/amd64 openscad/wasm-base:latest \
-  emcmake cmake -B build -DEXPERIMENTAL=ON "$@"
+  emcmake cmake -B build -DEXPERIMENTAL=ON -DOPENSCAD_EXPERIMENTAL=render-colors "$@"
 docker run --rm  -v "$OPENSCAD_DIR":/src:rw --platform=linux/amd64 openscad/wasm-base:latest \
   cmake --build build -j2
 
@@ -22,5 +22,6 @@ mkdir -p libs/openscad-wasm
 
 cp "$OPENSCAD_DIR/build/openscad.wasm" libs/openscad-wasm/
 cp "$OPENSCAD_DIR/build/openscad.js" libs/openscad-wasm/
+cp -r "$OPENSCAD_DIR/resources/locale" libs/openscad-wasm/
 cp "$OPENSCAD_DIR/build/openscad.wasm.map" libs/openscad-wasm/ || true
 ( cd libs && zip -r ../dist/openscad-wasm.zip openscad-wasm )
